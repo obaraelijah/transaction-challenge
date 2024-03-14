60eb5d3e7e92d020ce9eb70f949d9d81272a13cd
obaraelijah <elijahobara357@gmail.com>
2024-03-14 19:17:45 +0300

    chore: more ideas impl

--- src/tx.rs
+++ src/tx.rs
@@ -1,13 +1,25 @@
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
 
+#[derive(Deserialize, Clone, Copy)]
 pub struct Tx {
-
+    pub ty: TxType,
 }
 
+#[derive(Deserialize, Clone, Copy)]
 pub enum TxType {
+    #[serde(rename = "deposit")]
     Deposit,
+
+    #[serde(rename = "withdrawal")]
     Withdrawal,
+
+    #[serde(rename = "dispute")]
+    Dispute,
+
+    #[serde(rename = "resolve")]
     Resolve,
+
+    #[serde(rename = "chargeback")]
     Chargeback,
 }
