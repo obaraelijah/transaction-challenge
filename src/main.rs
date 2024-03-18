df5a723444b7f07afed67ea7c304a5b37b862d04
obaraelijah <elijahobara357@gmail.com>
2024-03-18 12:45:21 +0300

    feat: Implemented CSV processing pipeline for reading, processing, and writing data

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use engine::Engine;
 use tx::Tx;
 
 use anyhow::Result;
+use std::fs::File;
 use std::io::{self, Read, Write};
 use std::path::PathBuf;
 use clap::Parser;
@@ -29,6 +30,32 @@ fn write_cols(writer: &mut csv::Writer<&mut dyn Write>) -> Result<()> {
 
     Ok(())
 }
-fn main() {
-    unimplemented!()
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    file: PathBuf,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let mut file = File::open(&args.file)?;
+    let tx_stream = stream_records_from_reader(&mut file);
+    let mut stdout = io::stdout();
+    let mut writer = csv::Writer::from_writer(&mut stdout as &mut dyn Write);
+    let mut engine = Engine::new();
+
+    for tx in tx_stream {
+        if let Err(err) = engine.apply(tx?) {
+            eprintln!("{}", err);
+        }
+    }
+
+    write_cols(&mut writer)?;
+    for account in engine.accounts() {
+        writer.serialize(account)?;
+    }
+
+    writer.flush()?;
+    Ok(())
 }
